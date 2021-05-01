135db7945e640b12d4865dbd2512b2eb4b0306b3
Krupa Bhavsar <krups4444@gmail.com>
2021-05-01 13:47:32 +0530

    update

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
 var btnTranslate = document.querySelector("#btn-translate");
+var textInput = document.querySelector("#txt-input");
+
+console.log(textInput)
+
 btnTranslate.addEventListener("click", function clickEventHandler(){
-    console.log("clicked..!")
+    console.log("clicked..!");
+    console.log("input", textInput.value);
 })
